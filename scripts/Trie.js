132526ca530ca2a059a047104d6ce59004294040
Laura Caroselli <lcaroselli@outlook.com>
2017-07-17 23:02:30 -0600

    Insert method on trie

--- scripts/Trie.js
+++ scripts/Trie.js
@@ -10,10 +10,38 @@ export default class Trie {
   };
 
   insert(string) {
+    const node = new Node();
+
+    if(!this.root) {
+      this.root = node;
+    }
+
     [...string].forEach((el) => {
       if(!this.root.children[el]) {
-        this.root.children[el] = new Node(el);
+        this.root.children[el] = new Node();
+        this.root.children[el].letter = [el];
+        this.root = this.root.children[el];
       }
     })
+    console.log(JSON.stringify(this.root, null, 4));
   }
+
+  // insert(data) {
+  //   const node = new Node()
+  //
+  //   if (!this.root) {
+  //     this.root = node;
+  //   }
+  //
+  //   let letters = [...data];
+  //   let currentNode = this.root;
+  //
+  //   for(let i = 0; i < letters.length; i++) {
+  //     currentNode.children[letters[i]] = new Node();
+  //     currentNode.children[letters[i]].letter = letters[i];
+  //     currentNode = currentNode.children[letters[i]];
+  //   }
+  //   console.log(JSON.stringify(this.root, null, 4));
+  // }
+
 };
